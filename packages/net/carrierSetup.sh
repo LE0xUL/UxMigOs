74e17e86dc289f1131e26d3d1c700039239b96ca
Leonardo Urrego <trecetp@gmail.com>
2020-03-16 13:27:59 -0500

    Add 3G connection files

---
+++ packages/net/carrierSetup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo 22 > /sys/class/gpio/unexport
+echo 22 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio22/direction
+echo 0 > /sys/class/gpio/gpio22/value
+echo 1 > /sys/class/gpio/gpio22/value
+sleep 1
+echo 0 > /sys/class/gpio/gpio22/value
+#echo "Modem configured"
+sleep 6
+exit 0
