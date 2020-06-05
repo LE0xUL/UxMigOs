c0d09ab1d69d6b0dde258045ea5093a0dd20d6c4
Leonardo Urrego <trecetp@gmail.com>
2020-06-05 13:09:38 -0500

    Change execution of 3g connection

--- packages/migscripts/files/carrierConnect.sh
+++ packages/migscripts/files/carrierConnect.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 /usr/sbin/pppd defaultroute replacedefaultroute usepeerdns debug connect "/usr/sbin/chat -V -f /root/migstate/carrierFile.bkp" noauth /dev/ttyAMA0 nodetach 115200
-EXITVAL=$? 
-exit $EXITVAL
+EXITVAL=$?
+
+if [[ 8 -eq ${EXITVAL} ]]; then
+    /usr/bin/carrierSetup.sh
+    /usr/sbin/pppd defaultroute replacedefaultroute usepeerdns debug connect "/usr/sbin/chat -V -f /root/migstate/carrierFile.bkp" noauth /dev/ttyAMA0 nodetach 115200
+    EXITVAL=$?
+fi
+
+exit ${EXITVAL}
--- packages/migscripts/files/carrierSetup.sh
+++ packages/migscripts/files/carrierSetup.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-echo 22 > /sys/class/gpio/unexport
+echo 22 > /sys/class/gpio/unexport 2>/dev/null
+echo 26 > /sys/class/gpio/unexport 2>/dev/null
 echo 22 > /sys/class/gpio/export
+echo 26 > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio22/direction
+echo out > /sys/class/gpio/gpio26/direction
 echo 0 > /sys/class/gpio/gpio22/value
+echo 1 > /sys/class/gpio/gpio26/value
+sleep 0.2
+echo 0 > /sys/class/gpio/gpio26/value
 echo 1 > /sys/class/gpio/gpio22/value
 sleep 1
 echo 0 > /sys/class/gpio/gpio22/value
-#echo "Modem configured"
-sleep 6
+
 exit 0
